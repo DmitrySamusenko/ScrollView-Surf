2f0b97d1fa8976dc52b8c1f0b3b5f8dd307eacae
Dmitry Samusenko <royalitymean@gmail.com>
2023-02-08 21:45:14 +0300

    - firstCommit

--- ScrollView Surf/ViewController.swift
+++ ScrollView Surf/ViewController.swift
@@ -6,14 +6,55 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+struct Program {
+    var titleOfProgram: String
 }
 
+class ViewController: UIViewController {
+    @IBOutlet private var scrollView: UIScrollView!
+    @IBOutlet weak var eduProgCV: UICollectionView!
+    let programs: [Program] = [
+        Program(titleOfProgram: "iOS"),
+        Program(titleOfProgram: "Android"),
+        Program(titleOfProgram: "Flutter"),
+        Program(titleOfProgram: "Design"),
+        Program(titleOfProgram: "Business"),
+        Program(titleOfProgram: "iOS"),
+        Program(titleOfProgram: "Android"),
+        Program(titleOfProgram: "Flutter"),
+        Program(titleOfProgram: "Design")
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fixScrollViewBag()
+        registerNibs()
+    }
+
+    private func registerNibs() {
+           eduProgCV.register(UINib(nibName: EduProgCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: EduProgCollectionViewCell.identifier)
+       }
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return programs.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: EduProgCollectionViewCell.identifier, for: indexPath) as! EduProgCollectionViewCell
+        //cell.backgroundColor = .red
+        return cell
+    }
+    
+    
+    private func fixScrollViewBag() {
+        if let top = UIApplication.shared.windows.first?.safeAreaInsets.top {
+            scrollView.contentInset.top = -top
+        }
+        if let bottom = UIApplication.shared.windows.first?.safeAreaInsets.bottom {
+            scrollView.contentInset.bottom = -bottom
+        }
+    }
+}
